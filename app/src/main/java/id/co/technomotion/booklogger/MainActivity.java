dd5eced091a92886356db31d286a9897c092cfc3
omayib <omayib@gmail.com>
2015-05-19 06:08:19 +0700

    memasangkan arrayadapter dengan listview

--- app/src/main/java/id/co/technomotion/booklogger/MainActivity.java
+++ app/src/main/java/id/co/technomotion/booklogger/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends ActionBarActivity {
 
         //meng-inisiasi arrayadapter
         adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,listOfBook);
+        listViewBook.setAdapter(adapter);
 
 
     }
