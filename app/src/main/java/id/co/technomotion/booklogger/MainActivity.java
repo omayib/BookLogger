05526614ce5bb7a0c43152023ecb03020f409344
omayib <omayib@gmail.com>
2015-05-19 01:06:32 +0700

    menginisiasi view edittext, button dan listview berdasarkan id-nya

--- app/src/main/java/id/co/technomotion/booklogger/MainActivity.java
+++ app/src/main/java/id/co/technomotion/booklogger/MainActivity.java
@@ -4,14 +4,25 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
+    Button btnSimpan;
+    ListView listViewBook;
+    EditText editTextInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listViewBook= (ListView) findViewById(R.id.listView_output);
+        editTextInput= (EditText) findViewById(R.id.editText_input);
+        btnSimpan= (Button) findViewById(R.id.btn_simpan);
+
     }
 
 
